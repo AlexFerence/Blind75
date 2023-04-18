d9affbe114b7bd620ebdddcf3294d8cace42a5fe
AlexFerence <alexference23@gmail.com>
2023-04-18 18:45:28 -0400

    Circular array finally working

---
+++ src/dailies/Candies.java
@@ -0,0 +1,13 @@
+package dailies;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Candies {
+
+  public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+    return null;
+  }
+
+}
--- src/fastslowpointers/CircularArray.java
+++ src/fastslowpointers/CircularArray.java
@@ -5,46 +5,53 @@ import java.util.Set;
 
 public class CircularArray {
 
-  public static boolean circularArrayLoop(int[] nums) {
+  private static int nextIndex(int[] nums, int index) {
+    int n = nums.length;
+    int nextIndex = (index + nums[index]) % n;
+    if (nextIndex < 0) {
+      nextIndex += n;
+    }
+    return nextIndex;
+  }
 
+  public static boolean circularArrayLoop(int[] nums) {
     for (int i = 0; i < nums.length; i++) {
       int slow = i;
-      int fast = i;
-      fast = (fast + nums[fast]) % nums.length;
-      if (fast < 0) {
-        fast = fast + nums.length;
-      }
+      int fast = nextIndex(nums, i);
+      boolean foundLoop = false;
 
-      boolean foundEnd = false;
+      boolean isPositive = nums[fast] >= 0;
 
       while (slow != fast) {
-
-        int oldFast = fast;
-        fast = (fast + nums[fast]) % nums.length;
-        if (fast < 0) {
-          fast = fast + nums.length;
+        if (fast == nextIndex(nums,fast)) {
+          break;
         }
-        if (oldFast == fast) break;
+        fast = nextIndex(nums, fast);
+        boolean newIsPositive1 = nums[fast] >= 0;
 
-        oldFast = fast;
-        fast = (fast + nums[fast]) % nums.length;
-        if (fast < 0) {
-          fast = fast + nums.length;
-        }
-        if (oldFast == fast) break;
+        if (newIsPositive1 != isPositive) break;
 
-        slow = (slow + nums[slow]) % nums.length;
-        if (slow < 0) {
-          slow = slow + nums.length;
+        if (fast == nextIndex(nums,fast)) break;
+
+        fast = nextIndex(nums, fast);
+
+        boolean newIsPositive2 = nums[fast] >= 0;
+
+        if (newIsPositive2 != newIsPositive1) break;
+
+        slow = nextIndex(nums, slow);
+        if (fast == slow) {
+          foundLoop = true;
+          break;
         }
       }
+      if (foundLoop) return true;
     }
-
     return false;
   }
 
   public static void main(String[] args) {
-    int[] arr = {-1,-2,-3,-4,-5,6};
+    int[] arr = {1,-1,5,1,4};
     boolean res = circularArrayLoop(arr);
     System.out.println(res);
   }
