55fb1fccb49f164bf79c5aeef9bf9b8069261228
AlexFerence <alexference23@gmail.com>
2023-07-07 11:12:12 -0700

    DP starting

--- src/dynamicprogramming/CoinChange.java
+++ src/dynamicprogramming/CoinChange.java
@@ -5,15 +5,24 @@ import java.util.Arrays;
 public class CoinChange {
 
   public static int coinChange(int[] coins, int total) {
-
-    int[] dp = new int[total];
+    int[] dp = new int[total + 1];
     Arrays.fill(dp, -1);
-
-
-
-    // Write your code here
-
-    return -1;
+    for (int i = 0; i <= total; i++) {
+      int tempMin = Integer.MAX_VALUE;
+      for (int coin : coins) {
+        if (coin == i) {
+          dp[i] = 1;
+          break;
+        }
+        else if (i - coin > 0 && dp[i - coin] > 0) {
+          tempMin = Math.min(tempMin, 1 + dp[i - coin]);
+        }
+      }
+      if (dp[i] != 1 && tempMin != Integer.MAX_VALUE) {
+        dp[i] = tempMin;
+      }
+    }
+    return dp[total];
   }
 
   public static void main(String[] args) {
